a98bb1732c0878e835b2896963c04e30a72ce8d6
Nixiamoon <nvl.nixia@gmail.com>
2026-09-20 00:20:24 +0700

    Update DashboardAdmin.tsx

--- src/components/admin/dashboard/DashboardAdmin.tsx
+++ src/components/admin/dashboard/DashboardAdmin.tsx
@@ -20,7 +20,6 @@ import moonLogo from '../../../assets/moon-logo.svg';
 import IDCardModule from '../employee/IDCardModule';
 import '../../../styles/admin/id-card.css';
 import { useTranslation } from '../../../locales/LanguageContext';
-import SiDebarFloatingNavigator from '../../common/SiDebarFloatingNavigator';
 type Karyawan = {
   id: string;
   id_karyawan?: string;
@@ -520,14 +519,12 @@ function Heading({
   onAction?: () => void;
 }) {
   return (
-  <>
-    <SiDebarFloatingNavigator />
+    <div className="page-heading">
+      <div>
+        <h1>{title}</h1>
+        <p>{desc}</p>
+      </div>
 
-    <div className="si-debar-page">
-      {/* isi SI Debar */}
-    </div>
-  </>
-);
       {action && (
         <button className="primary" onClick={onAction}>
           {action}
